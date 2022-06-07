76f1274465cd0384d47bb21bdc7650c85ce08468
EIJI <eiji.vinicius@gmail.com>
2022-06-06 21:55:34 -0300

    subindo aula 6

---
+++ aula-6js.js
@@ -0,0 +1,53 @@
+
+ // comentário // 
+    // E entrada ---> P processamento ---> S saida
+
+    //Algoritimo 
+    
+    var nome = "Edir";   // "" aspas é STRING texto
+    var idade = 24;  // variavel numerica - NUMBER - integer (inteiro)
+    var peso = 43.7; // NUMBER - float 
+    var acordado = true; // valor boleano BOOLEAN ou BOLL ( so aceita VERDADEIRO ou FALSO)
+
+  // alert("Ola " + nome + "!");
+
+
+ // > - maior 
+ // < - menor 
+ // == - igual
+ // >= - maior igual
+ // <= - menor igual
+ // != - diferente
+
+
+  if (idade > peso) 
+  {
+       console.log("verdade");
+  } else{
+       console.log("falso"); 
+  }
+
+
+ // var texto = prompt("digite algo");
+ // console.log(texto);
+
+
+  //exercicio 1
+  // pepça 2 numeros e retorne o maior
+
+
+var x = prompt("digite um numero");
+var y = prompt("digite o segundo numero");
+var z = prompt("digite o terceiro numero");
+
+if (x > y)
+{
+    console.log(x);
+}
+else (x < y)
+{
+    console.log(y);
+}
+
+
+//peça 3 nuumeros e dizer o maior e tambem o menor 
