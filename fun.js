a0295544d7ccdffab1cc6bf88a72306adbdbe102
EIJI <eiji.vinicius@gmail.com>
2022-06-09 17:37:23 -0300

    tratando evento de travar letras no text

--- fun.js
+++ fun.js
@@ -17,3 +17,34 @@ function clique()
 
     console.log(resultado);
 }
+
+
+
+function somar() 
+{
+    var input1 = document.getElementById("numero-1");
+    
+
+    var input2 = document.getElementById("numero-2");
+    
+    var valor = soma(input1.value, input2.value);
+
+    var resultado = document.getElementById("resultadoid");
+    resultado.innerHTML = valor;
+
+}
+
+function tratar(evento)
+{
+
+    if (evento.keyCode <= 48 || evento.keyCode >= 57)
+    {
+        // evita o evento padrão
+        evento.preventDefault();
+        
+    }
+}
+
+var input1 = document.getElementById("numero-1");
+
+input1.addEventListener("keydown", tratar);
